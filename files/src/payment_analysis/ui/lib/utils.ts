48e8a2c33d999be92e465e71911752d66b583fac
anakin-skynnet <arielhsaa@hotmail.es>
2026-02-09 17:45:34 +0000

    latest_version

---
+++ files/src/payment_analysis/ui/lib/utils.ts
@@ -0,0 +1,8 @@
+/** Tailwind class merge utility (clsx + tailwind-merge). */
+
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
